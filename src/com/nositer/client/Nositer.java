22f22579ac59f5e6d3c126bc33b0f2ffeb4163da
Henry <Henry@optimus>
2011-01-13 08:33:03 -0800

    m

--- src/com/nositer/client/Nositer.java
+++ src/com/nositer/client/Nositer.java
@@ -1,152 +1,84 @@
 package com.nositer.client;
 
-import com.nositer.shared.FieldVerifier;
+import com.extjs.gxt.ui.client.Style.LayoutRegion;
+import com.extjs.gxt.ui.client.util.Margins;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.layout.AccordionLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
+import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Nositer implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+	private TopPanel topPanel;
+	private LeftPanel leftPanel;
+	private MainPanel mainPanel;
+	private LayoutContainer layoutContainer;
+	
+	public LayoutContainer getLayoutContainer() {
+		return layoutContainer;
 	}
+	public void setLayoutContainer(LayoutContainer layoutContainer) {
+		this.layoutContainer = layoutContainer;
+	}
+	public MainPanel getMainPanel() {
+		return mainPanel;
+	}
+	public void setMainPanel(MainPanel mainPanel) {
+		this.mainPanel = mainPanel;
+	}
+	
+	public TopPanel getTopPanel() {
+		return topPanel;
+	}
+	public void setTopPanel(TopPanel topPanel) {
+		this.topPanel = topPanel;
+	}
+	public LeftPanel getLeftPanel() {
+		return leftPanel;
+	}
+	public void setLeftPanel(LeftPanel leftPanel) {
+		this.leftPanel = leftPanel;
+	}
+	public void onModuleLoad() {
+		
+		LayoutContainer layoutContainer = new LayoutContainer();
+		BorderLayout borderLayout = new BorderLayout();
+		layoutContainer.setLayout(borderLayout);
+		
+		BorderLayoutData topPanelData = new BorderLayoutData(LayoutRegion.NORTH, 200);  
+		topPanel = new TopPanel(this);		
+		layoutContainer.add(topPanel, topPanelData);
+		
+		BorderLayoutData leftPanelData = new BorderLayoutData(LayoutRegion.WEST, 100);  
+		leftPanel = new LeftPanel(this);
+		layoutContainer.add(leftPanel, leftPanelData);
+		
+		
+		BorderLayoutData eastPanelData = new BorderLayoutData(LayoutRegion.EAST, 100);  
+		ContentPanel eastPanel = new ContentPanel();
+		layoutContainer.add(eastPanel, eastPanelData);
+		
+		BorderLayoutData southPanelData = new BorderLayoutData(LayoutRegion.SOUTH, 100);  
+		ContentPanel southPanel = new ContentPanel();
+		layoutContainer.add(southPanel, southPanelData);
+		
+		BorderLayoutData mainPanelData = new BorderLayoutData(LayoutRegion.CENTER);  
+		mainPanelData.setMargins(new Margins(5,5,5,5));  
+		mainPanelData.setSplit(true);
+		mainPanelData.setCollapsible(true);
+		mainPanel = new MainPanel(this);
+		layoutContainer.add(mainPanel, mainPanelData);
+		layoutContainer.setStyleAttribute("padding", "10px");  
+	
+	
+		RootPanel.get().add(layoutContainer);
+	}
+
 }
