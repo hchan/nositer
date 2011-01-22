fa7ca03268d18b6bd3b5d0be3d6b65ed2af09521
unknown <e52ymsg@.na.corp.mckesson.com>
2011-01-21 18:01:25 -0800

    'a'

--- src/com/nositer/client/register/Register.java
+++ src/com/nositer/client/register/Register.java
@@ -4,12 +4,18 @@ import com.extjs.gxt.ui.client.Style.LayoutRegion;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.Viewport;
+import com.extjs.gxt.ui.client.widget.form.FieldSet;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.Radio;
+import com.extjs.gxt.ui.client.widget.form.RadioGroup;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.CenterLayout;
+import com.extjs.gxt.ui.client.widget.layout.VBoxLayout;
+import com.extjs.gxt.ui.client.widget.layout.VBoxLayoutData;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.nositer.client.left.LeftPanel;
 import com.nositer.client.main.MainPanel;
@@ -22,7 +28,7 @@ public class Register implements EntryPoint {
 	private MainPanel mainPanel;
 	private LayoutContainer layoutContainer;
 	private FormPanel formPanel;
-	
+
 	public FormPanel getFormPanel() {
 		return formPanel;
 	}
@@ -62,48 +68,135 @@ public class Register implements EntryPoint {
 	public void setTopPanel(TopPanel topPanel) {
 		this.topPanel = topPanel;
 	}
-	
+
+	private Viewport viewport;
 	public void init() {
-		Viewport viewport = new Viewport();
-		viewport.setLayout(new BorderLayout());
-		initLayoutContainer();		
-		BorderLayoutData borderLayoutData = new BorderLayoutData(LayoutRegion.CENTER);
-		viewport.add(layoutContainer, borderLayoutData);
-		borderLayoutData.setMargins(new Margins(1));
+		viewport = new Viewport() {
+			@Override
+			protected void onRender(Element parent, int index) {
+			
+				super.onRender(parent, index);
+				viewport.setLayout(new BorderLayout());
+				initLayoutContainer();		
+				BorderLayoutData borderLayoutData = new BorderLayoutData(LayoutRegion.CENTER);
+				viewport.add(layoutContainer, borderLayoutData);
+				borderLayoutData.setMargins(new Margins(1));
+			}
+		};
+		
+		
 		instance = this;
 		RootPanel.get().add(viewport);		
 	}
 
-
-
 	public void initLayoutContainer() {
-		layoutContainer = new LayoutContainer();	
-		BorderLayout borderLayout = new BorderLayout();
-		layoutContainer.setLayout(borderLayout);
-		BorderLayoutData topLayoutData = new BorderLayoutData(LayoutRegion.NORTH);  
-		topPanel = new TopPanel(topLayoutData);		
-		BorderLayoutData mainLayoutData = new BorderLayoutData(LayoutRegion.CENTER);  
-		mainPanel = new MainPanel(mainLayoutData);
-		populateMainPanel();
-		layoutContainer.add(topPanel, topLayoutData);		
-		layoutContainer.add(mainPanel, mainLayoutData);	
+		layoutContainer = new LayoutContainer() {
+			@Override
+			protected void onRender(Element parent, int index) {
 			
-	}
+				super.onRender(parent, index);
+				BorderLayout borderLayout = new BorderLayout();
+				layoutContainer.setLayout(borderLayout);
+				BorderLayoutData topLayoutData = new BorderLayoutData(LayoutRegion.NORTH);  
+				topPanel = new TopPanel(topLayoutData);		
+				BorderLayoutData mainLayoutData = new BorderLayoutData(LayoutRegion.CENTER);  
+				
+				layoutContainer.add(topPanel, topLayoutData);		
+				populateMainPanel(mainLayoutData);
+				layoutContainer.add(mainPanel, mainLayoutData);	
+			}
+		};
 	
-	private void populateMainPanel() {		
-		initFormPanel();
-		mainPanel.add(formPanel);
+
+	}
+
+	private void populateMainPanel(BorderLayoutData mainLayoutData) {		
+		mainPanel = new MainPanel(mainLayoutData) {
+			@Override
+			protected void onRender(Element parent, int pos) {
+			
+				super.onRender(parent, pos);
+				initFormPanel();
+				mainPanel.add(formPanel);
+			};
+		};
+	
+		
+	}
+
+	private String getRequiredFieldStyle() {
+		return "color: red; font-weight: bold";
 	}
 
 	private void initFormPanel() {
-		formPanel = new FormPanel();
-		formPanel.setHeading("Registration");
-		formPanel.setFrame(true);
-		TextField<String> firstName = new TextField<String>();  
-		firstName.setFieldLabel("* Name");  
-		firstName.setLabelStyle("color: red");
-		formPanel.add(firstName);
-		formPanel.setWidth(500);
+		formPanel = new FormPanel() {
+			@Override
+			protected void onRender(Element target, int index) {
+				super.onRender(target, index);  
+				formPanel.setLabelWidth(150);
+				formPanel.setHeading("Registration");
+				formPanel.setFrame(true);
+				TextField<String> firstName = new TextField<String>();  
+				firstName.setFieldLabel("* Firstname");  
+				firstName.setLabelStyle(getRequiredFieldStyle());
+
+				TextField<String> lastName = new TextField<String>();  
+				lastName.setFieldLabel("* Lastname");  
+				lastName.setLabelStyle(getRequiredFieldStyle());
+
+				TextField<String> login = new TextField<String>();  
+				login.setFieldLabel("* Login name");  
+				login.setLabelStyle(getRequiredFieldStyle());
+
+				TextField<String> password = new TextField<String>();  
+				password.setFieldLabel("* Password");  
+				password.setLabelStyle(getRequiredFieldStyle());
+				password.setPassword(true);
+
+				TextField<String> passwordAgain = new TextField<String>();  
+				passwordAgain.setFieldLabel("* Password Again");  
+				passwordAgain.setLabelStyle(getRequiredFieldStyle());
+				passwordAgain.setPassword(true);
+
+				FieldSet location = new FieldSet();
+
+
+				location.setStyleName("registerLocation");
+				location.setHeading("* Location");
+
+
+
+				LayoutContainer locationLayoutContainer = new LayoutContainer() {
+					@Override
+					protected void onRender(Element parent, int index) {
+						super.onRender(parent, index);  
+						RadioGroup country = new RadioGroup();
+						Radio can = new Radio();
+						can.setBoxLabel("Canada");
+						can.setValue(true);
+						Radio usa = new Radio();
+						usa.setBoxLabel("USA");
+
+						country.add(can);
+						country.add(usa);
+						add(country, new VBoxLayoutData(new Margins(5, 0, 0, 10)));
+					}
+				};
+				locationLayoutContainer.setLayout(new VBoxLayout());
+				locationLayoutContainer.setAutoHeight(true);
+
+				location.add(locationLayoutContainer);
+
+				formPanel.add(firstName);
+				formPanel.add(lastName);
+				formPanel.add(login);
+				formPanel.add(password);
+				formPanel.add(passwordAgain);
+				formPanel.add(location);
+				formPanel.setWidth(500);
+			};
+		};
+		
 	}
 
 }
