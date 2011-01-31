d5715e2ade7b0284f04456d48fa17322adbdaee7
unknown <Henry@.(none)>
2011-01-31 01:00:50 -0800

    'a'

---
+++ test/com/nositer/sandbox/ImageResizer.java
@@ -0,0 +1,113 @@
+package com.nositer.sandbox;
+
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import sun.awt.image.BufferedImageGraphicsConfig;
+
+/**
+ * High-Quality Image Resize with Java
+ *
+ * Following two articles are source for this program
+ *
+ * http://www.componenthouse.com/article-20
+ * http://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
+ *
+ * @author ashwink
+ */
+public class ImageResizer{
+
+	public static void main(String[] args) {
+		try {
+			
+
+			BufferedImage image = ImageIO
+			.read(new File("c:/temp/unknownavatar.jpeg"));
+					
+
+			ImageIO.write(
+					//resizeTrick(image, image.getWidth(), image.getHeight()),
+					resizeTrick(image, 200, 200),
+					"png",
+					new File("c:/temp/unknownavatar.png"));
+							
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static BufferedImage resize(BufferedImage image, int width,
+			int height) {
+		int type = image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image
+				.getType();
+		BufferedImage resizedImage = new BufferedImage(width, height, type);
+		Graphics2D g = resizedImage.createGraphics();
+		g.setComposite(AlphaComposite.Src);
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.drawImage(image, 0, 0, width, height, null);
+		g.dispose();
+		return resizedImage;
+	}
+
+	private static BufferedImage resizeTrick(BufferedImage image, int width,
+			int height) {
+		image = createCompatibleImage(image);
+		image = resize(image, width, height);
+
+		/**
+		 * If you want to blur image
+		 */
+		// image = blurImage(image);
+		return resize(image, width, height);
+	}
+
+	public static BufferedImage blurImage(BufferedImage image) {
+		float ninth = 1.0f / 9.0f;
+		float[] blurKernel = { ninth, ninth, ninth, ninth, ninth, ninth, ninth,
+				ninth, ninth };
+
+		Map map = new HashMap();
+		map.put(RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		map.put(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+		map.put(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		RenderingHints hints = new RenderingHints(map);
+		BufferedImageOp op = new ConvolveOp(new Kernel(3, 3, blurKernel),
+				ConvolveOp.EDGE_NO_OP, hints);
+		return op.filter(image, null);
+	}
+
+	private static BufferedImage createCompatibleImage(BufferedImage image) {
+		GraphicsConfiguration gc = BufferedImageGraphicsConfig.getConfig(image);
+		int w = image.getWidth();
+		int h = image.getHeight();
+		BufferedImage result = gc.createCompatibleImage(w, h,
+				Transparency.TRANSLUCENT);
+		Graphics2D g2 = result.createGraphics();
+		g2.drawRenderedImage(image, null);
+		g2.dispose();
+		return result;
+	}
+
+}
