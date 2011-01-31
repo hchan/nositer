42748a4b193ba4747018050314e5d40f6860feda
unknown <Henry@.(none)>
2011-01-30 17:58:23 -0800

    'a'

---
+++ src/com/nositer/client/util/ImageHelper.java
@@ -0,0 +1,10 @@
+package com.nositer.client.util;
+
+public class ImageHelper {
+	public static final String IMAGEBASEDIR = "/image";
+	public static final String USERBASEDIR = IMAGEBASEDIR + "/u";
+	public static final String USERAVATAR = USERBASEDIR + "/avatar.png";
+	public static final String PUBLICBASEDIR = "/public/image";
+	public static final String UNKNOWNAVATAR = PUBLICBASEDIR + "/unknownavatar.png";
+	public static final String GROUPBASEDIR = IMAGEBASEDIR + "/g";
+}
