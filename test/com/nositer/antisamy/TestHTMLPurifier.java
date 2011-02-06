6b5f536323538d0326a4cd92f7088a519c797a97
unknown <Henry@.(none)>
2011-02-05 21:29:08 -0800

    'a'

--- test/com/nositer/antisamy/TestHTMLPurifier.java
+++ test/com/nositer/antisamy/TestHTMLPurifier.java
@@ -6,7 +6,7 @@ public class TestHTMLPurifier {
 	
 	public static void main(String[] args) {
 		try {
-			System.out.println(HTMLPurifier.getCleanHTML("<SCRIPT>alert();</SCRIPT>a<div class=\"ccc\">ddd</div>"));
+			System.out.println(HTMLPurifier.getCleanHTML("llllllllll<span style=\"font-weight: bold;\">llllll</span>"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
