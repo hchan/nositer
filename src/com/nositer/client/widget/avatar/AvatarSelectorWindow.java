7d27251d547a7a164040e1831b0bd32322e24a83
unknown <e52ymsg@.na.corp.mckesson.com>
2011-02-18 11:22:26 -0800

    'a'

--- src/com/nositer/client/widget/avatar/AvatarSelectorWindow.java
+++ src/com/nositer/client/widget/avatar/AvatarSelectorWindow.java
@@ -1,18 +1,36 @@
 package com.nositer.client.widget.avatar;
 
 import com.extjs.gxt.ui.client.widget.Window;
+import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
+import com.nositer.client.util.GWTUtil;
 import com.nositer.client.widget.directorytree.FileDirectoryTreeGridContainer;
 
+
 public class AvatarSelectorWindow extends Window {
+	private FileDirectoryTreeGridContainer fileDirectoryTreeGridContainer;
+
 	public AvatarSelectorWindow() {
 		init();
 	}
 
 	private void init() {
-		FileDirectoryTreeGridContainer fileDirectoryTreeGridContainer = new FileDirectoryTreeGridContainer();
+		fileDirectoryTreeGridContainer = new FileDirectoryTreeGridContainer(true);
+		fileDirectoryTreeGridContainer.setLayout(new FlowLayout(0));
+		
 		setHeading("Select Avatar");
 		add(fileDirectoryTreeGridContainer);
 		setModal(true);
 		setBlinkModal(true);
+		
 	}
+	
+	@Override
+	protected void onResize(int width, int height) {		
+		//super.onResize(width, height);
+		fileDirectoryTreeGridContainer.setSize(width, height);
+		fileDirectoryTreeGridContainer.getContentPanel().setSize(width-12, height);
+		
+	}
+	
+	
 }
