ecf719347321e290a55d06990a05334ecaa4d87e
unknown <e52ymsg@.na.corp.mckesson.com>
2011-02-28 15:43:13 -0800

    'a'

--- src/com/nositer/client/createiwantto/CreateIWantTo.java
+++ src/com/nositer/client/createiwantto/CreateIWantTo.java
@@ -4,8 +4,11 @@ import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayout;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayoutData;
 import com.nositer.client.main.MainPanel;
@@ -14,17 +17,11 @@ import com.nositer.client.widget.Resizable;
 
 public class CreateIWantTo extends LayoutContainer implements Resizable {
 
-	private Label iWantToLabel;
+
 	private Label headingLabel;
 	private ContentPanel contentPanel;
-	
-	public Label getiWantToLabel() {
-		return iWantToLabel;
-	}
-
-	public void setiWantToLabel(Label iWantToLabel) {
-		this.iWantToLabel = iWantToLabel;
-	}
+	private FormPanel formPanel;
+	private TextField<String> iWantTo;
 
 	public CreateIWantTo() {
 		init();
@@ -37,12 +34,20 @@ public class CreateIWantTo extends LayoutContainer implements Resizable {
 		contentPanel.setHeaderVisible(false);
 		VBoxLayout layout = new VBoxLayout();
 
+		
 		contentPanel.setLayout(layout);
 		headingLabel = new Label("Create I Want To ...");
 		headingLabel.setStyleName("formHeading");
 		contentPanel.add(headingLabel, new VBoxLayoutData(new Margins (5,5,5,5)));
-		iWantToLabel = new Label("I want to ");
-		contentPanel.add(iWantToLabel, new VBoxLayoutData(new Margins (5,5,5,5)));
+		
+		formPanel = new FormPanel();
+		formPanel.setBodyBorder(false);
+		formPanel.setHeaderVisible(false);
+		formPanel.setLabelSeparator("");
+		iWantTo = new TextField<String>();
+		iWantTo.setFieldLabel("I want to ");
+		formPanel.add(iWantTo, new FormData("100%"));
+		contentPanel.add(formPanel, new VBoxLayoutData());//new Margins (5,5,5,5)));
 		add(contentPanel);
 		resize(0,0);
 	}
@@ -52,6 +57,7 @@ public class CreateIWantTo extends LayoutContainer implements Resizable {
 		setSize(MainPanel.getInstance().getWidth(), MainPanel.getInstance().getHeight());
 		contentPanel.setWidth(MainPanel.getInstance().getWidth());
 		contentPanel.setHeight(MainPanel.getInstance().getHeight());
+		formPanel.setWidth(MainPanel.getInstance().getWidth() - 10);
 	}
 
 }
