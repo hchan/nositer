54ccd6ac57c139d89afefb9551c5750581b50337
unknown <e52ymsg@.na.corp.mckesson.com>
2011-06-10 11:33:02 -0700

    'a'

--- src/com/nositer/client/groupdiscussions/GrouptopicToolBar.java
+++ src/com/nositer/client/groupdiscussions/GrouptopicToolBar.java
@@ -6,8 +6,10 @@ import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.event.BaseEvent;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.WidgetComponent;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.layout.FillData;
@@ -26,15 +28,16 @@ import com.nositer.client.widget.Resizable;
 import com.nositer.client.widget.button.NextButton;
 import com.nositer.client.widget.button.PreviousButton;
 import com.nositer.client.widget.button.RefreshButton;
+import com.nositer.client.widget.messagebox.PromptMessageBox;
 
 public class GrouptopicToolBar extends ToolBar implements Resizable {
 	private GroupDiscussionsContainer groupDiscussionsContainer;
 	private Grouptopic grouptopic;
-	private RefreshButton refreshButton;
 	private NextButton nextButton;
 	private PreviousButton previousButton;
 	private LayoutContainer groupmessageTallyContainer;
-	private int cacheIndexOfGroupmessage;
+	private int cacheGroupmessageIndex;
+	private Label groupmessageIndexLabel;
 
 
 	public GrouptopicToolBar(GroupDiscussionsContainer groupDiscussionsContainer, Grouptopic grouptopic) {
@@ -47,7 +50,7 @@ public class GrouptopicToolBar extends ToolBar implements Resizable {
 	private void init() {
 		previousButton = new PreviousButton();
 		nextButton = new NextButton();
-		refreshButton = new RefreshButton();
+		groupmessageIndexLabel = new Label();
 		//setAlignment(HorizontalAlignment.CENTER);
 
 		add(previousButton);
@@ -62,25 +65,44 @@ public class GrouptopicToolBar extends ToolBar implements Resizable {
 	}
 
 	private void addDefaultListeners() {
-		previousButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+		final AsyncCallback<Groupmessage> calback = new AsyncCallback<Groupmessage>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				GWTUtil.log("", caught);
+			}
 
 			@Override
+			public void onSuccess(Groupmessage result) {
+				GroupmessagePanel groupmessagePanel = new GroupmessagePanel(groupDiscussionsContainer, result);
+				groupmessagePanel.show();
+			}
+
+		};
+
+
+		previousButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+			@Override
 			public void handleEvent(BaseEvent be) {
-				AsyncCallback<Groupmessage> calback = new AsyncCallback<Groupmessage>() {
+				ServiceBroker.groupService.getGroupmessage(grouptopic.getId(), cacheGroupmessageIndex-1, calback);
+			}
+		});
 
+		nextButton.addListener(Events.OnClick, new Listener<BaseEvent>() {
+			@Override
+			public void handleEvent(BaseEvent be) {
+				ServiceBroker.groupService.getGroupmessage(grouptopic.getId(), cacheGroupmessageIndex+1, calback);
+			}
+		});
+		
+		groupmessageIndexLabel.addListener(Events.OnClick, new Listener<BaseEvent>() {
+			@Override
+			public void handleEvent(BaseEvent be) {
+				PromptMessageBox.show("", "Enter the message index you would like to view", new Integer(cacheGroupmessageIndex).toString(), new Listener<MessageBoxEvent>()  {
 					@Override
-					public void onFailure(Throwable caught) {
-						GWTUtil.log("", caught);
+					public void handleEvent(MessageBoxEvent be) {
+						ServiceBroker.groupService.getGroupmessage(grouptopic.getId(),  new Integer(be.getValue()), calback);
 					}
-
-					@Override
-					public void onSuccess(Groupmessage result) {
-						GroupmessagePanel groupmessagePanel = new GroupmessagePanel(groupDiscussionsContainer, result);
-						groupmessagePanel.show();
-					}
-					
-				};
-				ServiceBroker.groupService.getGroupmessage(grouptopic.getId(), cacheIndexOfGroupmessage-1, calback);
+				});
 			}
 		});
 	}
@@ -93,24 +115,24 @@ public class GrouptopicToolBar extends ToolBar implements Resizable {
 		//groupmessageTallyContainer.add(new Label(), new MarginData(0, widthOfToolBar - 145, 0, 0));
 		groupmessageTallyContainer.add(new Label(), new MarginData(0, widthOfToolBar/2 - 120, 0, 0));
 		groupmessageTallyContainer.add(new Label("Message "));
-		Label tallyCount = new Label();
-		int indexOfGroupmessage = getIndexOfGroupmessage(grouptopic);
-		tallyCount.setText(new Integer(indexOfGroupmessage).toString());
-		tallyCount.setStyleName("tallyCount");
-		
-		groupmessageTallyContainer.add(tallyCount);
+
+		int groupmessageIndex = getIndexOfGroupmessage(grouptopic);
+		groupmessageIndexLabel.setText(new Integer(groupmessageIndex).toString());
+		groupmessageIndexLabel.setStyleName("tallyCount");
+
+		groupmessageTallyContainer.add(groupmessageIndexLabel);
 		groupmessageTallyContainer.add(new Label(" of " + grouptopic.getGroupmessages().size()));
-		
-		if (indexOfGroupmessage == grouptopic.getGroupmessages().size()) {
+
+		if (groupmessageIndex == grouptopic.getGroupmessages().size()) {
 			nextButton.disable();
 		}
-		
-		if (indexOfGroupmessage == 1) {
+
+		if (groupmessageIndex == 1) {
 			previousButton.disable();
 		}
-		
-		 cacheIndexOfGroupmessage = indexOfGroupmessage;
-		
+
+		cacheGroupmessageIndex = groupmessageIndex;
+
 		//groupmessageTallyContainer.add(new Label(), new MarginData(0, widthOfToolBar/3 - 70, 0, 0));
 		layout();
 	}
--- src/com/nositer/client/widget/messagebox/PromptMessageBox.java
+++ src/com/nositer/client/widget/messagebox/PromptMessageBox.java
@@ -1,13 +1,39 @@
 package com.nositer.client.widget.messagebox;
 
+import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.MessageBoxEvent;
+import com.extjs.gxt.ui.client.widget.Dialog;
 import com.extjs.gxt.ui.client.widget.MessageBox;
+import com.extjs.gxt.ui.client.widget.MessageBox.MessageBoxType;
 
 public class PromptMessageBox {
 	public static void show (String title, String msg, Listener<MessageBoxEvent> callback) {
 		MessageBox.prompt(title, msg, callback);
 	}
 
-	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void show (String title, String msg, final String textFieldValue, Listener<MessageBoxEvent> callback) {
+		final MessageBox messageBox = new MessageBox();
+
+		messageBox.setTitle(title);
+
+		messageBox.setMessage(msg);
+		messageBox.setType(MessageBoxType.PROMPT);
+		messageBox.setButtons(Dialog.OKCANCEL);
+		if (callback != null) {
+			messageBox.addCallback(callback);
+		}
+
+		Dialog dialog = messageBox.getDialog();
+		dialog.addListener(Events.Activate, new Listener() {
+			@Override
+			public void handleEvent(BaseEvent be) {
+				messageBox.getTextBox().setValue(textFieldValue);
+				messageBox.getTextBox().selectAll();
+			}
+		});
+		dialog.show();
+	}
 }
